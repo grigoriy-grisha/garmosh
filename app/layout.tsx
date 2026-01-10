eafaad0cb1e0a2689ca240a59dd8e98cf5e5d860
voblin <lowtrace19@tuta.com>
2026-01-10 21:38:10 +0300

    metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { fontSans } from '@/lib/fonts';
 import Header from '@/components/header/Header';
 import { LightboxProvider } from '@/components/lightbox';
 import type { Metadata, Viewport } from 'next';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://darya-garmash.ru'),
@@ -52,6 +53,29 @@ export default function RootLayout({
           <Header />
           {children}
         </LightboxProvider>
+
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+        >{`(function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106198229', 'ym');
+
+        ym(106198229, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}</Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/106198229"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
       </body>
     </html>
   );
